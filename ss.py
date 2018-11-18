fa0add25c6eeae63b524f8aa7465da372b7cb25c
sehoven <shoven@ualberta.ca>
2018-11-18 14:45:21 -0700

    Cleaned up code and added more practice puzzles.

--- ss.py
+++ ss.py
@@ -1,22 +1,9 @@
 import sys
 from numpy import copy
 
-print(sys.argv)
-
-f = open(sys.argv[1], "r").read().split()
-
-board = [[None] * 9 for x in range(9)]
-
-for lineNum in range(len(f)):
-    for charNum in range(len(f[lineNum])):
-        ch = f[lineNum][charNum]
-        board[lineNum][charNum] = int(ch) if ch != "." else None
-
-## I'm going to write code between these lines only :p
-board = copy(board)
-print(board)
-
-# find next empty space (from specified row and col)
+"""
+Find next empty space in board (from specified row and col).
+"""
 def findNextEmpty(board, row, col):
     for newRow in range(row, 9):
         # can't search earlier than col if on row
@@ -26,56 +13,48 @@ def findNextEmpty(board, row, col):
             if not currentSquare:
                 return newRow, newCol
 
-## End
+"""
+Check if cell with specified value in board at [x, y] is valid.
 
-
-## I will write code between these lines only :p
-
-def cellIsValid(x, y, value, board):
+Checks if the value has already been seen in any row, column, or square of the board.
+"""
+def isCellValid(board, x, y, value):
     isValid = True
-    print(x, y, value)
 
     # check rows
     for cell in board[x]:
         if cell == value:
             isValid = False
-            print("ROW FALSE")
             break
 
-    # print("ROW:",board[x])
-
     # check columns
-    # only continue if valid so far
+    # only continue if valid so far (row is valid)
     if isValid:
-        # print("COLUMN:")
         for i in range(0, 9):
-            # print(board[i][y])
             if board[i][y] == value:
                 isValid = False
-                print("COLUMN FALSE")
                 break
 
     # check square=
-    # only continue if valid so far
+    # only continue if valid so far (row and column are valid)
     if isValid:
         startRow = x - (x % 3)
         startColumn = y - (y % 3)
-        # print("SQUARE:")
         for i in range (startRow, startRow + 3):
             if isValid:
                 for j in range(startColumn, startColumn + 3):
                     if board[i][j] == value:
                         isValid = False
-                        print("SQUARE FALSE")
                         break
             else:
                 # if we've found that the square is not valid, break out early
                 break
 
     return isValid
-
-# should ensure board is validated before passing in
-def solveBoard(board, row=0, col=0):
+"""
+Solves the board through backtracking method.
+"""
+def solveBoard(board, row = 0, col = 0):
     board = copy(board)
 
     # find next empty space (from specified row and col)
@@ -87,8 +66,8 @@ def solveBoard(board, row=0, col=0):
 
     # place a value and validate it
     result = False
-    for number in range(1, 9+1):
-        if not cellIsValid(newRow, newCol, number, board):
+    for number in range(1, 10):
+        if not isCellValid(board, newRow, newCol, number):
             continue
         board[newRow][newCol] = number
         result = solveBoard(board, newRow, newCol)
@@ -96,4 +75,25 @@ def solveBoard(board, row=0, col=0):
             break
     return result
 
-print("solution:", solveBoard(board))
+def main():
+    if len(sys.argv) > 2:
+        print("Expects one argument: filename")
+        return
+
+    try:
+        f = open(sys.argv[1], "r").read().split()
+    except:
+        print("Could not open file. Check your file path and name.")
+        return
+
+    board = [[None] * 9 for x in range(9)]
+
+    for lineNum in range(len(f)):
+        for charNum in range(len(f[lineNum])):
+            ch = f[lineNum][charNum]
+            board[lineNum][charNum] = int(ch) if ch != "." else None
+
+    print("solution:\n", solveBoard(board))
+
+if __name__ == "__main__":
+    main()
